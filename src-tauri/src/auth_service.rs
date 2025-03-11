749629fbe0e40d0ba9237a46295ad781b9613ef5
Otsuki Hitoshi <hitochan777@gmail.com>
2025-03-12 00:33:00 +0900

    Refactor

--- src-tauri/src/auth_service.rs
+++ src-tauri/src/auth_service.rs
@@ -1,8 +1,14 @@
+use anyhow::Result;
+use oauth2::basic::{BasicErrorResponseType, BasicTokenType};
 use oauth2::{
     basic::BasicClient, AuthUrl, AuthorizationCode, ClientId, CsrfToken, PkceCodeChallenge,
     PkceCodeVerifier, RedirectUrl, Scope, TokenUrl,
 };
-use oauth2::{reqwest, AccessToken, TokenResponse};
+use oauth2::{
+    reqwest, AccessToken, EmptyExtraTokenFields, EndpointNotSet, EndpointSet,
+    RevocationErrorResponseType, StandardErrorResponse, StandardRevocableToken,
+    StandardTokenIntrospectionResponse, StandardTokenResponse, TokenResponse,
+};
 use serde::Deserialize;
 use std::env;
 use std::sync::Arc;
@@ -20,6 +26,37 @@ struct CallbackQuery {
     state: CsrfToken,
 }
 
+type Client = oauth2::Client<
+    StandardErrorResponse<BasicErrorResponseType>,
+    StandardTokenResponse<EmptyExtraTokenFields, BasicTokenType>,
+    StandardTokenIntrospectionResponse<EmptyExtraTokenFields, BasicTokenType>,
+    StandardRevocableToken,
+    StandardErrorResponse<RevocationErrorResponseType>,
+    EndpointSet,
+    EndpointNotSet,
+    EndpointNotSet,
+    EndpointNotSet,
+    EndpointSet,
+>;
+
+fn create_oauth_client(port: u16) -> Result<Client> {
+    let client_id =
+        ClientId::new(env::var("CLIENT_ID").map_err(|_| anyhow::anyhow!("Missing CLIENT_ID"))?);
+    let auth_url =
+        AuthUrl::new(env::var("AUTH_URL").map_err(|_| anyhow::anyhow!("Missing AUTH_URL"))?)
+            .map_err(|_| anyhow::anyhow!("Invalid AUTH_URL"))?;
+    let token_url =
+        TokenUrl::new(env::var("TOKEN_URL").map_err(|_| anyhow::anyhow!("Missing TOKEN_URL"))?)
+            .map_err(|_| anyhow::anyhow!("Invalid TOKEN_URL"))?;
+
+    let client = BasicClient::new(client_id)
+        .set_auth_uri(auth_url)
+        .set_token_uri(token_url)
+        .set_redirect_uri(RedirectUrl::new(format!("http://localhost:{port}"))?);
+
+    Ok(client)
+}
+
 pub async fn authenticate() -> anyhow::Result<AccessToken> {
     let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel();
     let port = start_with_config(
@@ -31,24 +68,6 @@ pub async fn authenticate() -> anyhow::Result<AccessToken> {
             let _ = tx.send(url);
         },
     )?;
-
-    fn create_oauth_client(port: u16) -> Result<BasicClient, anyhow::Error> {
-        let client_id =
-            ClientId::new(env::var("CLIENT_ID").map_err(|_| anyhow::anyhow!("Missing CLIENT_ID"))?);
-        let auth_url =
-            AuthUrl::new(env::var("AUTH_URL").map_err(|_| anyhow::anyhow!("Missing AUTH_URL"))?)
-                .map_err(|_| anyhow::anyhow!("Invalid AUTH_URL"))?;
-        let token_url =
-            TokenUrl::new(env::var("TOKEN_URL").map_err(|_| anyhow::anyhow!("Missing TOKEN_URL"))?)
-                .map_err(|_| anyhow::anyhow!("Invalid TOKEN_URL"))?;
-
-        let client = BasicClient::new(client_id)
-            .set_auth_uri(auth_url)
-            .set_token_uri(token_url)
-            .set_redirect_uri(RedirectUrl::new(format!("http://localhost:{port}"))?);
-
-        Ok(client)
-    }
 
     let client = create_oauth_client(port)?;
 
